86f31f26ae8b8ca70438b57662386930fffd8b48
Greg Kobzeff <gregkobzeff.dev@gmail.com>
2019-08-24 19:12:50 -0700

    Added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true,
+        "mocha": true,
+    },
+    "settings": {
+        "react": {
+            "version": "^16.9.0"
+        }
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "react/display-name": 0,
+        "react/prop-types": 0,
+    }
+};
