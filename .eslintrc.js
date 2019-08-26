f008ab26cc24f1a0583648dff3335c9d51c0fa6e
Greg Kobzeff <gregkobzeff.dev@gmail.com>
2019-08-25 18:22:56 -0700

    Fixed eslint parse error on arrow functions

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
         "Atomics": "readonly",
         "SharedArrayBuffer": "readonly"
     },
+    "parser": "babel-eslint",
     "parserOptions": {
         "ecmaFeatures": {
             "jsx": true
