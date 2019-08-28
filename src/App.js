c3bf2b7ea91b32a50ef4a626493f21a01f4760b8
Greg Kobzeff <gregkobzeff.dev@gmail.com>
2019-08-27 20:32:06 -0700

    Refactored to add NavigationBar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React, { Component } from "react";
-import { Link, withRouter } from "react-router-dom";
-import { Nav, Navbar, Form, Button } from "react-bootstrap";
-import { LinkContainer } from "react-router-bootstrap";
-import SymbolSelector from "./components/SymbolSelector";
+import { withRouter } from "react-router-dom";
+import NavigationBar from "./components/NavigationBar"
 import Routes from "./Routes";
 import './App.css';
-import logo from './images/logo.png';
 
 class App extends Component {
 
@@ -13,31 +10,12 @@ class App extends Component {
     super(props);
     this.state = {
       isAuthenticated: false,
-      isAuthenticating: true,
-      symbol: null
+      isAuthenticating: true
     };
   }
 
   userHasAuthenticated = authenticated => {
     this.setState({ isAuthenticated: authenticated });
-  }
-
-  handleSearch = async event => {
-    event.preventDefault();
-    if (this.state.symbol == null) return; //no valid selection
-    this.props.history.push(`/stocks/${this.state.symbol}`);
-  }
-
-  handleSymbolKeyDown = async event => {
-    if (event.key === "Enter") {
-      this.handleSearch(event);
-    }
-  }
-
-  handleSymbolChange = selected => {
-    this.setState({
-      symbol: selected[0]
-    });
   }
 
   render() {
@@ -49,41 +27,7 @@ class App extends Component {
 
     return (
       <div className="App container">
-        <Navbar bg="light">
-          <Navbar.Brand>
-            <Link to="/">
-              <img
-                src={logo}
-                width="30"
-                height="30"
-                className="d-inline-block align-top"
-                alt="Earnings Ready logo"
-              />
-            </Link>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Nav>
-            <LinkContainer to="/overview">
-              <Nav.Link>Overview</Nav.Link>
-            </LinkContainer>
-            <LinkContainer to="/calendar">
-              <Nav.Link>Calendar</Nav.Link>
-            </LinkContainer>
-            <LinkContainer to="/heatmap">
-              <Nav.Link>Heat Map</Nav.Link>
-            </LinkContainer>
-            <LinkContainer to="/test">
-              <Nav.Link>Test</Nav.Link>
-            </LinkContainer>
-          </Nav>
-          <Nav className="ml-auto">
-            <Form inline>
-              <SymbolSelector placeholder="Enter a symbol" onChange={this.handleSymbolChange} onKeyDown={this.handleSymbolKeyDown} />
-              <Button className="search" onClick={this.handleSearch}>Search</Button>
-            </Form>
-          </Nav>
-        </Navbar>
-
+        <NavigationBar props={childProps} />
         <Routes childProps={childProps} />
       </div>
     );
---
+++ src/components/NavigationBar.js
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+import { Nav, Navbar, Form, Button } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import { Link, withRouter } from "react-router-dom";
+import SymbolSelector from "./SymbolSelector";
+import logo from '../images/logo.png';
+import './NavigationBar.css';
+
+class NavigationBar extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      symbol: null
+    };
+  }
+
+  handleSearch = async event => {
+    event.preventDefault();
+    if (this.state.symbol == null) return; //no valid selection
+    this.props.history.push(`/stocks/${this.state.symbol}`);
+  }
+
+  handleSymbolKeyDown = async event => {
+    if (event.key === "Enter") {
+      this.handleSearch(event);
+    }
+  }
+
+  handleSymbolChange = selected => {
+    this.setState({
+      symbol: selected[0]
+    });
+  }
+
+  render() {
+    return (
+      <Navbar bg="light">
+        <Navbar.Brand>
+          <Link to="/">
+            <img
+              src={logo}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              alt="Earnings Ready logo"
+            />
+          </Link>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Nav>
+          <LinkContainer to="/overview">
+            <Nav.Link>Overview</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/calendar">
+            <Nav.Link>Calendar</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/heatmap">
+            <Nav.Link>Heat Map</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/test">
+            <Nav.Link>Test</Nav.Link>
+          </LinkContainer>
+        </Nav>
+        <Nav className="ml-auto">
+          <Form inline>
+            <SymbolSelector placeholder="Enter a symbol" onChange={this.handleSymbolChange} onKeyDown={this.handleSymbolKeyDown} />
+            <Button className="search" onClick={this.handleSearch}>Search</Button>
+          </Form>
+        </Nav>
+      </Navbar>
+    );
+  }
+
+}
+
+export default withRouter(NavigationBar);
