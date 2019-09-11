184eddde559644ace0220ad3bd0c088fbc959648
Greg Kobzeff <gregkobzeff.dev@gmail.com>
2019-09-11 15:02:41 -0700

    Moved existing test into tests subfolder

--- tests/App.test.js
+++ tests/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { MemoryRouter } from "react-router-dom";
-import App from './App';
+import App from '../src/App';
 
-it('renders without crashing', () => {
+it('renders App without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<MemoryRouter><App /></MemoryRouter>, div);
   ReactDOM.unmountComponentAtNode(div);
